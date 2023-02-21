29298c3abc8cf03baa647f20705b9248dd118182
Junaid Malik <junmalik109@gmail.com>
2023-02-21 18:28:15 +0000

    app dir basic components added

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Chat GPT Next JS</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <div></div>
+
+      <div className="">{children}</div>
+
+      </body>
+    </html>
+  )
+}
