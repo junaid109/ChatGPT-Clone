7178e5ae5a8dbcf8056ebab3ff9926a69a3e1a70
Junaid Malik <junmalik109@gmail.com>
2023-02-21 16:42:49 +0000

    Added tailwindcss config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Global } from '@emotion/react'
-import xw from 'xwind'
 
 function App({ Component, pageProps }) {
   return (
@@ -10,7 +9,6 @@ function App({ Component, pageProps }) {
       </Head>
       <Global
         //tailwind base styles + keyframes + ring and shadow classes variables  ... to global styles
-        styles={xw`XWIND_BASE XWIND_GLOBAL`}
       />
       <Component {...pageProps} />
     </>
