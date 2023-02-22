0629857fe26e7e0be3b456573046b763a6e2db1c
Junaid Malik <junmalik109@gmail.com>
2023-02-22 14:19:47 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images : {
+    domains: ["links.papareact.com"]
+  },
+
   experimental: {
     optimizeFonts: true,
     appDir: "app",
